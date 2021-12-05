c1bc1819b8eb8a1fe9f5166bd2e155021a3bb221
Hesam <mohebi.hesam@gmail.com>
2021-12-05 20:29:47 +0330

    problmen zero between numbers has been solved

--- ctype_source/total.c
+++ ctype_source/total.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-int total(int *x, int n){
+int total(double *x, int n){
 	int i;
 	double count = 0;
-	for(i = 0 ; i<2*n;i++){
+	for(i = 0 ; i<n;i++){
 		count += x[i];
-		printf("%d\n", x[i]);
+		printf("%d : %f\n", i, x[i]);
 	}
 
 	return count;
--- ctype_source/total.py
+++ ctype_source/total.py
@@ -1,13 +1,33 @@
 import ctypes
 import numpy as np
 
-_lib = ctypes.CDLL('./sample.so')
+
 
 def totalCtypes(arr, n):
-    return _lib.total(arr.ctypes.data_as(ctypes.c_void_p), n)
+
+    _lib = ctypes.CDLL('./sample.so')
+    _lib.total.argtypes = [ctypes.POINTER(ctypes.c_double), ctypes.c_int]
+    array_type = ctypes.c_double * n
+
+    # return _lib.total(arr.ctypes.data_as(ctypes.c_void_p), n)
+    return _lib.total(array_type(*arr), ctypes.c_int(n))
 
 n = 100
 
-x = np.arange(n)
+
+# x = np.arange(n, dtype=np.double)
+# x = np.random.random((n,)).tolist()
+
+# x = []
+# for i in range(n):
+#     x.append(i)
+
+x = np.random.random((n,)).tolist()
+
+
 print(x)
 print(totalCtypes(x,n))
+
+
+
+# print(our_function([1,2,-3,4,-5,6]))
