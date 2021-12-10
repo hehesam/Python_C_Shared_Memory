2a8f3b71ced552961c5475a5234c3d871dc6d9e3
Hesam <mohebi.hesam@gmail.com>
2021-12-10 16:26:39 +0330

    speech recognition with google

--- Voice_Recognition/voic.py
+++ Voice_Recognition/voic.py
@@ -1,1 +1,16 @@
-print("this is me ")
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print('Speak : ')
+    audio = r.listen(source)
+
+
+    try:
+        text = r.recognize_google(audio)
+        print('You said : {}'.format(text))
+        print(type(text))
+
+    except:
+        print('did not hear it ')
