379b701ff2016e407f8eff0849eb629eab70b8bd
Hesam <mohebi.hesam@gmail.com>
2021-12-11 00:12:52 +0330

    giving order using  voice to shared memory from python to c

---
+++ shared_voice/c_python.c
@@ -0,0 +1,135 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/shm.h>
+#include <string.h>
+
+#include "struct.h"
+
+// #include <string>
+
+// #include <iostream>
+
+// using namespace std;
+
+// struct Shared_Segment
+// {
+// 	int size;
+// 	double * numbers;
+	
+// };
+
+void array_printer(int * arr, int n){
+	printf("this is the array in c_ptyhon file \n");
+	for(int i=0;i<n*2;i++)
+		printf("%d \n",arr[i]);
+
+	// cout<<endl;
+	printf("\n");
+}
+
+
+int total_int(int * arr, int n){
+	array_printer(arr, n);
+	int * shared_memory;
+
+	int shmid;
+
+	shmid = shmget((key_t)1122, 1024, 0666|IPC_CREAT);
+
+	// printf("Key of shared memory is %d\n", shmid);
+
+	shared_memory = (int *)shmat(shmid, NULL, 0); // procces attached to shared memory segment
+
+	printf("Process attached at %p\n", shared_memory);
+
+	printf("i will now copy the arr in shared memory\n");
+
+	for(int i=0;i<n*2;i++)
+
+		shared_memory[i] = arr[i];
+
+	printf("the shared_memory should now be in full \n");
+
+	array_printer(shared_memory, n);
+
+	
+	return 1;
+
+}
+
+double total_double(double * arr, int n){
+	double * shared_memory;
+
+	int shmid;
+
+	shmid = shmget((key_t)1122, 1024, 0666|IPC_CREAT);
+
+	// printf("Key of shared memory is %d\n", shmid);
+
+	shared_memory = (double *)shmat(shmid, NULL, 0); // procces attached to shared memory segment
+
+	printf("Process attached at %p\n", shared_memory);
+
+	printf("i will now copy the arr in shared memory\n");
+
+	for(int i=0;i<n;i++){
+
+
+		shared_memory[i] = arr[i];
+		printf("index : %d has the value : %f\n",i, shared_memory[i] );
+	}
+
+	printf("the shared_memory should now be in full \n");
+
+	// array_printer(shared_memory, n);
+
+	
+	return 1;
+
+
+}
+
+double total_struct(double * arr, int n){
+
+	struct Shared_Segment * shared_memory;
+
+	int shmid;
+
+	shmid = shmget((key_t)1122, 1024, 0666|IPC_CREAT);
+
+	printf("Key of shared memory is %d\n", shmid);
+
+	shared_memory = shmat(shmid, NULL, 0); // procces attached to shared memory segment
+
+	printf("Process attached at %p\n", shared_memory);
+
+	printf("i will now copy the arr in shared memory numbers\n");
+
+	for(int i=0;i<n;i++){
+
+		// printf("value of arr is : %f\n", arr[i]);
+
+		shared_memory->numbers[i] = arr[i];
+
+
+		printf("index : %d has the value : %f\n",i, shared_memory->numbers[i] );
+	}
+	shared_memory->size = n;
+
+	printf("the shared_memory should now be in full \n");
+
+	// array_printer(shared_memory, n);
+
+	
+	return 1;
+
+}
+
+// int main(){
+// 	// double  arr[3];
+// 	// arr[0] = 1.01;
+// 	// arr[1] = 3.03;
+// 	// arr[2] = 2.02;
+// 	// total_struct(arr, 3);
+// }
--- shared_voice/voice_text.py
+++ shared_voice/voice_text.py
@@ -1,1 +1,27 @@
-#hi
+import speech_recognition as sr
+
+def speak():
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('Speak : ')
+        audio = r.listen(source)
+
+
+        try:
+            text = r.recognize_google(audio)
+            print('You said : {}'.format(text))
+            print(type(text))
+
+        except:
+            print('did not hear it ')
+
+        arr = text.split(" ")
+        nrr = []
+        for i in arr:
+            dd = float(i)
+            nrr.append(dd)
+
+        return nrr;
+
+
